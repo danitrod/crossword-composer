f2483f5f522d335cfea0baed9b7311a05af3906f
danitrod <danitrod@outlook.com>
2021-10-19 22:19:16 -0300

    enable ms viewing

--- ui/src/parallel/worker.js
+++ ui/src/parallel/worker.js
@@ -2,7 +2,6 @@ import init, { parSolve } from '../../../pkg/crossword';
 
 self.onmessage = ({ data }) => {
   init(data.wasm).then(() => {
-    console.log('worker', data.id, 'initialized');
     let result = parSolve(data.wordlist, data.grid, data.id, data.numThreads);
     postMessage(result);
   });
--- ui/src/status.js
+++ ui/src/status.js
@@ -1,37 +1,33 @@
 export class RunStatus {
-    constructor() {
-        this.started = null
-        this.ended = null
-    }
+  constructor() {
+    this.started = null;
+    this.ended = null;
+  }
 
-    start() {
-        this.started = new Date()
-        this.ended = null
-    }
+  start() {
+    this.started = new Date();
+    this.ended = null;
+  }
 
-    end() {
-        this.ended = new Date()
-    }
+  end() {
+    this.ended = new Date();
+  }
 
-    getRunning() {
-        return this.started && !this.ended
-    }
+  getRunning() {
+    return this.started && !this.ended;
+  }
 
-    getStatus() {
-        return this.started ? (this.ended ? 'Finished' : 'Running') : 'Not Started';
-    }
+  getStatus() {
+    return this.started ? (this.ended ? 'Finished' : 'Running') : 'Not Started';
+  }
 
-    getTime() {
-        if (!this.started) {
-            return '(not started)'
-        }
-        let end = this.ended || new Date()
-        let s = end.valueOf() - this.started.valueOf()
-
-        if (s > 1000) {
-            return `${Math.round(s / 1000)}s`
-        } else {
-            return `${s}ms`
-        }
+  getTime() {
+    if (!this.started) {
+      return '(not started)';
     }
+    let end = this.ended || new Date();
+    let s = end.valueOf() - this.started.valueOf();
+
+    return `${s}ms`;
+  }
 }
